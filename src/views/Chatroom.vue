a0189d1edd540c8fb8fdeb0b31fbde8ce8dc1d10
ryzen666 <htoohtetaung26@gmail.com>
2022-10-21 20:21:15 +0630

    navbar in chatroom and logout button

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    <h1>Chatroom</h1>
+  <div class="container">
+    <Navbar></Navbar>
   </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar'
 export default {
+  components: { Navbar },
 
 }
 </script>
